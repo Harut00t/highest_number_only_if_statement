eae6d65a3fff21b44475918a5efb668acb45625a
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-15 00:56:03 +0800

    added the fifth variable, it was not that hard but I think there is a bug in my system, haha

--- getting_the_highest_number_only_if_statement.py
+++ getting_the_highest_number_only_if_statement.py
@@ -3,7 +3,7 @@ var1 = int(input("insert a first number here: "))
 var2 = int(input("insert a second number here: "))
 var3 = int(input("insert a third number here: "))
 var4 = int(input("insert a fourth number here: "))
-#var5 = int(input("insert a fifth number here: "))
+var5 = int(input("insert a fifth number here: "))
 
 #create def find_largest_number and conditional statements
 #if var1 is larger than var2
@@ -24,28 +24,52 @@ var4 = int(input("insert a fourth number here: "))
             #if var3 larger than var5:
 #if any of the last variable are equal to one another!!
 
-def find_largest_number(var1, var2, var3, var4):
+def find_largest_number(var1, var2, var3, var4, var5):
     if var1 > var2:
         if var1 > var3:
             if var1 > var4:
-                return var1
+                if var1 > var5:
+                    return var1
+                else:
+                    return var5
             else:
-                return var4
+                if var4 > var5:
+                    return var4
+                else:
+                    return var5
         else: 
             if var3 > var4:
-                return var3
+                if var3 > var5:
+                    return var3
+                else: 
+                    return var5
             else:
-                return var4
+                if var4 > var5:
+                    return var4
+                else:
+                    return var5
     else:
         if var2 > var3:
             if var2 > var4:
-                return var2
+                if var2 > var5:
+                    return var2
+                else:
+                    return var5
             else:
-                return var4
+                if var4 > var5:
+                    return var4
+                else:
+                    return var5
         else:
             if var3 > var4:
-                return var3
+                if var3 > var5:
+                    return var3
+                else:
+                    return var5
             else:
-                return var4
-result = find_largest_number(var1, var2, var3, var4)
+                if var4 > var5:
+                    return var4
+                else:
+                    return var5
+result = find_largest_number(var1, var2, var3, var4, var5)
 print(f"The highest number is {result}")

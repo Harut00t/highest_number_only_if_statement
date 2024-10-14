3bffaeb1a35ffa7e7c6e43ef248d2ac05b77cf6b
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-15 00:44:28 +0800

    I made the conditional statements for 4 variables to test it out, it works but its really getting complicated.

--- getting_the_highest_number_only_if_statement.py
+++ getting_the_highest_number_only_if_statement.py
@@ -1,10 +1,9 @@
 #variables for input, making input to int number 1, 2, 3, 4 and 5
-
 var1 = int(input("insert a first number here: "))
 var2 = int(input("insert a second number here: "))
 var3 = int(input("insert a third number here: "))
 var4 = int(input("insert a fourth number here: "))
-var5 = int(input("insert a fifth number here: "))
+#var5 = int(input("insert a fifth number here: "))
 
 #create def find_largest_number and conditional statements
 #if var1 is larger than var2
@@ -25,3 +24,28 @@ var5 = int(input("insert a fifth number here: "))
             #if var3 larger than var5:
 #if any of the last variable are equal to one another!!
 
+def find_largest_number(var1, var2, var3, var4):
+    if var1 > var2:
+        if var1 > var3:
+            if var1 > var4:
+                return var1
+            else:
+                return var4
+        else: 
+            if var3 > var4:
+                return var3
+            else:
+                return var4
+    else:
+        if var2 > var3:
+            if var2 > var4:
+                return var2
+            else:
+                return var4
+        else:
+            if var3 > var4:
+                return var3
+            else:
+                return var4
+result = find_largest_number(var1, var2, var3, var4)
+print(f"The highest number is {result}")

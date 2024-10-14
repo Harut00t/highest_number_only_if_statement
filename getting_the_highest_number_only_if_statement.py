7a9ea72dc3d3cc0eb440b93b9f9a65eff2e5631a
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-15 00:25:33 +0800

    pseudocode, too complicated in my opinion, hope it works!

---
+++ getting_the_highest_number_only_if_statement.py
@@ -0,0 +1,20 @@
+#variables for input, making input to int number 1, 2, 3, 4 and 5
+#create def find_largest_number and conditional statements
+#if var1 is larger than var2
+    #if var1 is larger than var3:
+        #else:
+            #if var3 is larger than var4:
+                #if var3 is larger than var5:
+    #elif var1 is larger than var4:
+        #else:
+            #if var4 larger than var5
+    #elif var1 is larger than var5:
+#else:
+    #var2 is larger than var3:
+        #if var2 larger than var4:
+            #if var2 larger than var5:
+    #else:
+        #if var3 larger than var4:
+            #if var3 larger than var5:
+#if any of the last variable are equal to one another!!
+
